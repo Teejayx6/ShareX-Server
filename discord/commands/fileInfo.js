26e93b9e4b4b3bc11eb654d064ce26b09b2a317a
MILLION <apaolini900o@outlook.com>
2020-09-18 22:31:46 -0400

    adding fileinfo command

---
+++ discord/commands/fileInfo.js
@@ -0,0 +1,26 @@
+const { MessageEmbed } = require('discord.js-light');
+
+const userModel = require('../../models/user');
+
+let name = 'fileinfo';
+let aliases = ['fi'];
+let run = async (msg, args) => {
+    if (!args[0]) return msg.channel.send(new MessageEmbed()
+        .setTitle(`You must include the name of a file (Include the file extention).`)
+        .setColor('#e9172b'));
+
+    let uName = args[0];
+
+    let fileData = await userModel.findOne({ name: uName });
+
+    if (fileData == null) return msg.channel.send(new MessageEmbed()
+        .setTitle('File does not exist.')
+        .setColor('#e9172b'));
+
+    return msg.channel.send(new MessageEmbed()
+        .setTitle(`File: \`${uName}\``)
+        .setDescription(`Path: \`${fileData.path}\`\nUploader: \`${fileData.uploader}\`\nViews: \`${fileData.views}\`\nDate: \`${fileData.createdAt}\`\n`)
+        .setColor('#e9172b'));
+};
+
+module.exports = { name, aliases, run };
