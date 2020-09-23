0cc6b5a893e074b8ed64e7d1e08bc37532442713
MILLION <apaolini900o@outlook.com>
2020-09-23 14:12:30 -0400

    yes

--- discord/commands/ping.js
+++ discord/commands/ping.js
@@ -5,7 +5,7 @@ const { MessageEmbed } = require("discord.js-light");
 
 let name = 'ping';
 let aliases = ['fuckoff'];
-let run = async (msg, args) => {
+let run = async (msg, args, owner) => {
     return msg.channel.send(new MessageEmbed()
         .setTitle(`Ping! | ${msg.client.ws.ping}ms`)
         .setColor('#1eda61'));
