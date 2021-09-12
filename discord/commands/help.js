d7031d7a8ea38c884da74610e6c0e3f3f8192cad
Swiper <89560897+Teejayx6@users.noreply.github.com>
2021-09-11 22:26:41 -0500

    Update help.js

--- discord/commands/help.js
+++ discord/commands/help.js
@@ -7,7 +7,7 @@ let run = async (msg, args, owner) => {
 
     return msg.channel.send(new MessageEmbed()
         .setTitle(`Help`)
-        .setDescription(`**userinfo**: \`Gets Info On A User\`\n**fileinfo**: \`Gets Info On A File\`\n**ping**: \`Gets The Bots Ping\`\n**linkaccount**: \`Links Your Discord Account To Your h4x3r.xyz account\`\n**unlinkaccount**: \`Unlinks Your Discord Account From your h4x3r.xyz account\`\n**help**: \`Shows This Help Menu\``)
+        .setDescription(`**userinfo**: \`Gets Info On A User\`\n**fileinfo**: \`Gets Info On A File\`\n**ping**: \`Gets The Bots Ping\`\n**linkaccount**: \`Links Your Discord Account To Your h4x3r.xyz account\`\n**unlinkaccount**: \`Unlinks Your Discord Account From your h4x3r.xyz account\`\n**help**: \`Shows This Help Menu\`\n**delfile**: \`Deletes A Uploaded FIle\``)
         .setColor('#4693f2'));
 };
 
