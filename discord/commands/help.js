a827ad424b121436cc6a4713e01218a338a82d6c
Swiper <89560897+Teejayx6@users.noreply.github.com>
2021-09-11 22:25:10 -0500

    Added Help Command

---
+++ discord/commands/help.js
@@ -0,0 +1,14 @@
+const { MessageEmbed } = require('discord.js-light');
+
+
+let name = 'help';
+let aliases = ['h'];
+let run = async (msg, args, owner) => {
+
+    return msg.channel.send(new MessageEmbed()
+        .setTitle(`Help`)
+        .setDescription(`**userinfo**: \`Gets Info On A User\`\n**fileinfo**: \`Gets Info On A File\`\n**ping**: \`Gets The Bots Ping\`\n**linkaccount**: \`Links Your Discord Account To Your h4x3r.xyz account\`\n**unlinkaccount**: \`Unlinks Your Discord Account From your h4x3r.xyz account\`\n**help**: \`Shows This Help Menu\``)
+        .setColor('#4693f2'));
+};
+
+module.exports = { name, aliases, run };
