045c91e5743acc744ef754a3eacfa0885efc9e02
Swiper <89560897+Teejayx6@users.noreply.github.com>
2021-09-15 18:42:55 -0500

    Making Url SHortner CMD

---
+++ discord/commands/shortner.js
@@ -0,0 +1,8 @@
+var request = require("request")
+var fs = require("fs")
+const { MessageEmbed } = require('discord.js-light');
+
+
+let name = 'shortner';
+let aliases = ['s'];
+let run = async (msg, args, owner) => {
