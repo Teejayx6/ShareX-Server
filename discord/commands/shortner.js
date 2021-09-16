70a40a326f30f2f18650af9bce82f8fed7c1e705
Swiper <89560897+Teejayx6@users.noreply.github.com>
2021-09-15 19:05:19 -0500

    Idk TBH :sob:

--- discord/commands/shortner.js
+++ discord/commands/shortner.js
@@ -5,7 +5,12 @@ const { MessageEmbed } = require('discord.js-light');
 
 let name = 'shortner';
 let aliases = ['s'];
-let run = async (msg, args, owner) => {
+let run = async (msg, args) => {
 
 let url = args[0]; 
 let key = args[1];
+
+    if (!args[0]) return msg.channel.send(new MessageEmbed()
+        .setTitle(`You Must Include A URL.`)
+        .setColor('#4693f2'));
+
