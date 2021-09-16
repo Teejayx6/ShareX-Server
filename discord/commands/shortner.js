459c4914a5364793f2b19ef4becb01ed47e8823c
Swiper <89560897+Teejayx6@users.noreply.github.com>
2021-09-15 19:02:32 -0500

    Update shortner.js

--- discord/commands/shortner.js
+++ discord/commands/shortner.js
@@ -1,8 +1,11 @@
-var request = require("request")
-var fs = require("fs")
+const axios = require('axios');
+
 const { MessageEmbed } = require('discord.js-light');
 
 
 let name = 'shortner';
 let aliases = ['s'];
 let run = async (msg, args, owner) => {
+
+let url = args[0]; 
+let key = args[1];
